df321c616975a9a4dec9a0d27200d500e8935e23
yellowsunshine <sidnav999@gmail.com>
2021-12-29 15:50:21 +0000

    1550

--- src/test/java/com/automationpractice/cucumber/steps/OnlineShoppingSteps.java
+++ src/test/java/com/automationpractice/cucumber/steps/OnlineShoppingSteps.java
@@ -62,6 +62,7 @@ public class OnlineShoppingSteps {
         List<List<String>> data = table.raw();
         for (int i = 1; i < data.size(); i++) {
             new HomePage().navigateToHomePage();
+            //  getting data from row 1 and column 0
             new HomePage().selectItemFromMainMenu(data.get(i).get(0));
             new HomePage().selectItemsFromInnerMenu(data.get(i).get(1));
             new ProductPage().selectProduct(data.get(i).get(2));
@@ -74,6 +75,8 @@ public class OnlineShoppingSteps {
 
     @Then("^I shall validate shopping cart as below$")
     public void iShallValidateShoppingCartAsBelow(DataTable table) {
+        //convert data table to a raw table, this map will return a list of map (of string as key and string as value)
+        // raw methods return type is List<List<String>>
         List<List<String>> data1 = table.raw();
         for (int i = 1; i < 3; i++) {
             new ShoppingCartPage().clickOnShoppingCart();
@@ -116,12 +119,20 @@ public class OnlineShoppingSteps {
 
     @And("^I add below product to cart using Maps$")
     public void iAddBelowProductToCartUsingMaps(DataTable dataTable) {
+        //convert data table to map, this map will return a list of map (of string as key and string as value)
+        //first row acts as a header
         List<Map<String, String>> userList = dataTable.asMaps(String.class, String.class);
         for (int i = 0; i < userList.size(); i++) {
             new HomePage().navigateToHomePage();
+            //retrieving item from row (index number) of the column called category
+            //each and every row value will be mapped with respective column headers
+            //There are 3 rows, from row # 0 get me the value of the key called category
             new HomePage().selectItemFromMainMenu(userList.get(i).get("category"));
+            //There are 3 rows, from row # 0 get me the value of the key called subCategory
             new HomePage().selectItemsFromInnerMenu(userList.get(i).get("subCategory"));
+            //There are 3 rows, from row # 0 get me the value of the key called name
             new ProductPage().selectProduct(userList.get(i).get("name"));
+            //There are 3 rows, from row # 0 get me the value of the key called quantity
             new ProductPage().selectQuantity(userList.get(i).get("quantity"));
             new SummerDressesPage().addToCart();
             new ShoppingCartPage().clickOnContinueShoppingButton();
@@ -130,9 +141,11 @@ public class OnlineShoppingSteps {
 
     @Then("^I shall validate shopping cart as below using Maps$")
     public void iShallValidateShoppingCartAsBelowUsingMaps(DataTable dataTable) {
+        //convert data table to map, this map will return a list of map (of string as key and string as value)
         List<Map<String, String>> userList = dataTable.asMaps(String.class, String.class);
         for (int i = 0; i < userList.size(); i++)  {
             new ShoppingCartPage().clickOnShoppingCart();
+            //from row 0 get values of keys called name, model and quantity
             new ShoppingCartPage().verifyShoppingCart(userList.get(i).get("name"), userList.get(i).get("model"), userList.get(i).get("quantity"));
         }
     }
--- target/cucumber-report/cucumber.html/report.js
+++ target/cucumber-report/cucumber.html/report.js
@@ -7,36 +7,168 @@ formatter.feature({
   "keyword": "Feature"
 });
 formatter.before({
-  "duration": 7194110600,
+  "duration": 19738134300,
   "status": "passed"
 });
 formatter.scenario({
-  "line": 32,
-  "name": "TC-003 Add product to Online Cart and checkout using Maps",
+  "line": 4,
+  "name": "TC-001 Create New User/SignUp",
   "description": "",
-  "id": "signup,-sign-in-and-purchase-product;tc-003-add-product-to-online-cart-and-checkout-using-maps",
+  "id": "signup,-sign-in-and-purchase-product;tc-001-create-new-user/signup",
   "type": "scenario",
   "keyword": "Scenario",
   "tags": [
     {
-      "line": 31,
-      "name": "@Sample"
+      "line": 3,
+      "name": "@Regression"
     }
   ]
 });
 formatter.step({
-  "line": 33,
+  "line": 5,
+  "name": "I am on home page",
+  "keyword": "Given "
+});
+formatter.step({
+  "line": 6,
+  "name": "Navigate to SignUp page",
+  "keyword": "Then "
+});
+formatter.step({
+  "line": 7,
+  "name": "create an account with random username",
+  "rows": [
+    {
+      "cells": [
+        "James",
+        "Bond",
+        "abc12456",
+        "123 Abc Street",
+        "London",
+        "Florida",
+        "12345",
+        "01234567890"
+      ],
+      "line": 8
+    }
+  ],
+  "keyword": "And "
+});
+formatter.match({
+  "location": "OnlineShoppingSteps.iAmOnHomePage()"
+});
+formatter.result({
+  "duration": 164414500,
+  "status": "passed"
+});
+formatter.match({
+  "location": "OnlineShoppingSteps.navigateToSignUpPage()"
+});
+formatter.result({
+  "duration": 5763143800,
+  "status": "passed"
+});
+formatter.match({
+  "location": "OnlineShoppingSteps.createAnAccountWithRandomUsername(DataTable)"
+});
+formatter.result({
+  "duration": 20113384900,
+  "error_message": "org.openqa.selenium.NoSuchElementException: no such element: Unable to locate element: {\"method\":\"css selector\",\"selector\":\"#email_create\"}\n  (Session info: chrome\u003d96.0.4664.110)\nFor documentation on this error, please visit: https://selenium.dev/exceptions/#no_such_element\nBuild info: version: \u00274.0.0\u0027, revision: \u00273a21814679\u0027\nSystem info: host: \u0027KEYKIN1\u0027, ip: \u0027192.168.68.106\u0027, os.name: \u0027Windows 10\u0027, os.arch: \u0027amd64\u0027, os.version: \u002710.0\u0027, java.version: \u00271.8.0_121\u0027\nDriver info: org.openqa.selenium.chrome.ChromeDriver\nCommand: [baf48a76d4edaeb3f8996bf206efaaf4, findElement {using\u003did, value\u003demail_create}]\nCapabilities {acceptInsecureCerts: false, browserName: chrome, browserVersion: 96.0.4664.110, chrome: {chromedriverVersion: 96.0.4664.45 (76e4c1bb2ab46..., userDataDir: C:\\Users\\Kinjal\\AppData\\Loc...}, goog:chromeOptions: {debuggerAddress: localhost:63284}, javascriptEnabled: true, networkConnectionEnabled: false, pageLoadStrategy: normal, platform: WINDOWS, platformName: WINDOWS, proxy: Proxy(), se:cdp: ws://localhost:63284/devtoo..., se:cdpVersion: 96.0.4664.110, setWindowRect: true, strictFileInteractability: false, timeouts: {implicit: 0, pageLoad: 300000, script: 30000}, unhandledPromptBehavior: dismiss and notify, webauthn:extension:credBlob: true, webauthn:extension:largeBlob: true, webauthn:virtualAuthenticators: true}\nSession ID: baf48a76d4edaeb3f8996bf206efaaf4\r\n\tat sun.reflect.NativeConstructorAccessorImpl.newInstance0(Native Method)\r\n\tat sun.reflect.NativeConstructorAccessorImpl.newInstance(NativeConstructorAccessorImpl.java:62)\r\n\tat sun.reflect.DelegatingConstructorAccessorImpl.newInstance(DelegatingConstructorAccessorImpl.java:45)\r\n\tat java.lang.reflect.Constructor.newInstance(Constructor.java:423)\r\n\tat org.openqa.selenium.remote.codec.w3c.W3CHttpResponseCodec.createException(W3CHttpResponseCodec.java:200)\r\n\tat org.openqa.selenium.remote.codec.w3c.W3CHttpResponseCodec.decode(W3CHttpResponseCodec.java:133)\r\n\tat org.openqa.selenium.remote.codec.w3c.W3CHttpResponseCodec.decode(W3CHttpResponseCodec.java:53)\r\n\tat org.openqa.selenium.remote.HttpCommandExecutor.execute(HttpCommandExecutor.java:184)\r\n\tat org.openqa.selenium.remote.service.DriverCommandExecutor.invokeExecute(DriverCommandExecutor.java:164)\r\n\tat org.openqa.selenium.remote.service.DriverCommandExecutor.execute(DriverCommandExecutor.java:139)\r\n\tat org.openqa.selenium.remote.RemoteWebDriver.execute(RemoteWebDriver.java:559)\r\n\tat org.openqa.selenium.remote.ElementLocation$ElementFinder$2.findElement(ElementLocation.java:162)\r\n\tat org.openqa.selenium.remote.ElementLocation.findElement(ElementLocation.java:66)\r\n\tat org.openqa.selenium.remote.RemoteWebDriver.findElement(RemoteWebDriver.java:383)\r\n\tat org.openqa.selenium.remote.RemoteWebDriver.findElement(RemoteWebDriver.java:375)\r\n\tat org.openqa.selenium.support.pagefactory.DefaultElementLocator.findElement(DefaultElementLocator.java:70)\r\n\tat org.openqa.selenium.support.pagefactory.internal.LocatingElementHandler.invoke(LocatingElementHandler.java:39)\r\n\tat com.sun.proxy.$Proxy25.sendKeys(Unknown Source)\r\n\tat com.automationpractice.utility.Utility.pmSendTextToElement(Utility.java:56)\r\n\tat com.automationpractice.pages.AuthenticationPage.enterEmailInTheEmailAddressField(AuthenticationPage.java:43)\r\n\tat com.automationpractice.cucumber.steps.OnlineShoppingSteps.createAnAccountWithRandomUsername(OnlineShoppingSteps.java:34)\r\n\tat ✽.And create an account with random username(onlineshopping.feature:7)\r\n",
+  "status": "failed"
+});
+formatter.after({
+  "duration": 134736500,
+  "status": "passed"
+});
+formatter.before({
+  "duration": 11985643800,
+  "status": "passed"
+});
+formatter.scenario({
+  "line": 11,
+  "name": "TC-002 Sign In with newly created user credentials",
+  "description": "",
+  "id": "signup,-sign-in-and-purchase-product;tc-002-sign-in-with-newly-created-user-credentials",
+  "type": "scenario",
+  "keyword": "Scenario",
+  "tags": [
+    {
+      "line": 10,
+      "name": "@Regression"
+    }
+  ]
+});
+formatter.step({
+  "line": 12,
   "name": "I am on the Sign In Page",
   "keyword": "Given "
 });
 formatter.step({
-  "line": 34,
+  "line": 13,
   "name": "Login using newly create credentials",
   "keyword": "When "
 });
 formatter.step({
-  "line": 35,
-  "name": "I add below product to cart using Maps",
+  "line": 14,
+  "name": "I shall verify the address information in my address section",
+  "keyword": "Then "
+});
+formatter.match({
+  "location": "OnlineShoppingSteps.iAmOnTheSignInPage()"
+});
+formatter.result({
+  "duration": 15707414600,
+  "status": "passed"
+});
+formatter.match({
+  "location": "OnlineShoppingSteps.loginUsingNewlyCreateCredentials()"
+});
+formatter.result({
+  "duration": 14505328500,
+  "status": "passed"
+});
+formatter.match({
+  "location": "OnlineShoppingSteps.iShallVerifyTheAddressInformationInMyAddressSection()"
+});
+formatter.result({
+  "duration": 10338898300,
+  "status": "passed"
+});
+formatter.after({
+  "duration": 36100,
+  "status": "passed"
+});
+formatter.before({
+  "duration": 12552880200,
+  "status": "passed"
+});
+formatter.scenario({
+  "line": 17,
+  "name": "TC-003 Add product to Online Cart and checkout using Raw Table",
+  "description": "",
+  "id": "signup,-sign-in-and-purchase-product;tc-003-add-product-to-online-cart-and-checkout-using-raw-table",
+  "type": "scenario",
+  "keyword": "Scenario",
+  "tags": [
+    {
+      "line": 16,
+      "name": "@Regression"
+    }
+  ]
+});
+formatter.step({
+  "line": 18,
+  "name": "I am on the Sign In Page",
+  "keyword": "Given "
+});
+formatter.step({
+  "line": 19,
+  "name": "Login using newly create credentials",
+  "keyword": "When "
+});
+formatter.step({
+  "line": 20,
+  "name": "I add below product to cart",
   "rows": [
     {
       "cells": [
@@ -46,7 +178,7 @@ formatter.step({
         "model",
         "quantity"
       ],
-      "line": 36
+      "line": 21
     },
     {
       "cells": [
@@ -56,7 +188,7 @@ formatter.step({
         "demo_3",
         "2"
       ],
-      "line": 37
+      "line": 22
     },
     {
       "cells": [
@@ -66,14 +198,14 @@ formatter.step({
         "demo_1",
         "3"
       ],
-      "line": 38
+      "line": 23
     }
   ],
   "keyword": "And "
 });
 formatter.step({
-  "line": 39,
-  "name": "I shall validate shopping cart as below using Maps",
+  "line": 24,
+  "name": "I shall validate shopping cart as below",
   "rows": [
     {
       "cells": [
@@ -81,7 +213,7 @@ formatter.step({
         "model",
         "quantity"
       ],
-      "line": 40
+      "line": 25
     },
     {
       "cells": [
@@ -89,7 +221,7 @@ formatter.step({
         "SKU : demo_3",
         "2"
       ],
-      "line": 41
+      "line": 26
     },
     {
       "cells": [
@@ -97,18 +229,18 @@ formatter.step({
         "SKU : demo_1",
         "3"
       ],
-      "line": 42
+      "line": 27
     }
   ],
   "keyword": "Then "
 });
 formatter.step({
-  "line": 43,
+  "line": 28,
   "name": "I shall be able to Buy the product",
   "keyword": "Then "
 });
 formatter.step({
-  "line": 44,
+  "line": 29,
   "name": "I shall be able to Buy using cheque payment",
   "keyword": "And "
 });
@@ -116,46 +248,16 @@ formatter.match({
   "location": "OnlineShoppingSteps.iAmOnTheSignInPage()"
 });
 formatter.result({
-  "duration": 1579832200,
+  "duration": 13013959400,
   "status": "passed"
 });
 formatter.match({
   "location": "OnlineShoppingSteps.loginUsingNewlyCreateCredentials()"
 });
 formatter.result({
-  "duration": 1569189000,
+  "duration": 13267798900,
   "status": "passed"
 });
 formatter.match({
-  "location": "OnlineShoppingSteps.iAddBelowProductToCartUsingMaps(DataTable)"
-});
-formatter.result({
-  "duration": 14249739500,
-  "status": "passed"
-});
-formatter.match({
-  "location": "OnlineShoppingSteps.iShallValidateShoppingCartAsBelowUsingMaps(DataTable)"
-});
-formatter.result({
-  "duration": 4166018500,
-  "status": "passed"
-});
-formatter.match({
-  "location": "OnlineShoppingSteps.iShallBeAbleToBuyTheProduct()"
-});
-formatter.result({
-  "duration": 7312442900,
-  "status": "passed"
-});
-formatter.match({
-  "location": "OnlineShoppingSteps.iShallBeAbleToBuyUsingChequePayment()"
-});
-formatter.result({
-  "duration": 59671500,
-  "status": "passed"
-});
-formatter.after({
-  "duration": 95700,
-  "status": "passed"
-});
+  "location": "OnlineShoppingSteps.iAddBelowProductToCart(DataTable)"
 });
